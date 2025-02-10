4072454f9ff56a71b2206c21f0e3bca4c35d17e7
Jon O'Neill <jononeill@daleuw.com>
2025-02-10 00:15:22 +0000

    return ORI data to JonoTest page!

--- Helpers/SqlExtractor.cs
+++ Helpers/SqlExtractor.cs
@@ -2,9 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+//using System.Data.SqlClient;
 using System.Threading.Tasks;
-//using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Sandbox.Models.Windowpane;
 
 namespace Sandbox.Helpers
