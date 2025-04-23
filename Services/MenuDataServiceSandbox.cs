9bcb6ad047a4a32ef0496482bd16d50bc37d60da
Jono <jononeill@daleuw.com>
2025-04-23 22:50:08 +0100

    improvements to copy function tidying menu

--- Services/MenuDataServiceSandbox.cs
+++ Services/MenuDataServiceSandbox.cs
@@ -7,7 +7,7 @@ public class MenuDataServiceSandbox
         ),
         new MainMenuItems(
             type: "sub",
-            title: "Dashboards",
+            title: "ORI",
             icon: "",
             svg: "<svg xmlns='http://www.w3.org/2000/svg' class='w-6 h-6 side-menu__icon' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' d='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25' /></svg>",
             badgeValue: "",
@@ -42,14 +42,14 @@ public class MenuDataServiceSandbox
 					active: false,
 					dirChange: false
 				),
-				new MainMenuItems (
-					path: "/NavHist",
-					type: "link",
-					title: "NavHist",
-					selected: false,
-					active: false,
-					dirChange: false
-				),
+				//new MainMenuItems (
+				//	path: "/NavHist",
+				//	type: "link",
+				//	title: "NavHist",
+				//	selected: false,
+				//	active: false,
+				//	dirChange: false
+				//),
 			}
         ),
 
