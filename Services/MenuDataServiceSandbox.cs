2d8c23d2e566e90cb0e53df8a66e2089df6cdd92
Jono <jononeill@daleuw.com>
2025-05-06 08:42:03 +0100

    removing nav hist from menu

--- Services/MenuDataServiceSandbox.cs
+++ Services/MenuDataServiceSandbox.cs
@@ -42,18 +42,16 @@ public class MenuDataServiceSandbox
 					active: false,
 					dirChange: false
 				),
-				new MainMenuItems (
-					path: "NavHist",
-					type: "link",
-					title: "NavHist",
-					selected: false,
-					active: false,
-					dirChange: false
-				),
+				//new MainMenuItems (
+				//	path: "NavHist",
+				//	type: "link",
+				//	title: "NavHist",
+				//	selected: false,
+				//	active: false,
+				//	dirChange: false
+				//),
 			}
-        ),
-
-        
+        ),        
     };
 
     public List<MainMenuItems> GetMenuData()
