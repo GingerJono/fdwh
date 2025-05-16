f075bd37bf3dd4cd3dbc95d9abdaa3217cf636ca
x Foote <xfoote@daleuw.com>
2025-05-16 10:09:18 +0100

    Only show download buttons if there is data available

--- Models/Prism/RunModel.cs
+++ Models/Prism/RunModel.cs
@@ -21,5 +21,16 @@
         public List<AllocatedIncurredRecoveriesByORIPolicyEventClassYOAModel> AllocatedIncurredRecoveries { get; set; } = new();
         public List<AllocatedUltimateRecoveriesByORIPolicyEventClassYOAModel> AllocatedUltimateRecoveries { get; set; } = new();
         public List<AllocatedRecoveryModel> AllocatedRecoveriesMerged { get; set; } = new();
+
+        public bool HasOutput
+        {
+            get
+            {
+                return this.ORIPolIncurredClaimsMapping.Count != 0 || this.ORIPolUltimateClaimsMapping.Count != 0
+                     || this.IncurredClaimsByEvent.Count != 0 || this.UltimateClaimsByEvent.Count != 0
+                     || this.AllocatedPaidRecoveries.Count != 0 || this.AllocatedIncurredRecoveries.Count != 0
+                     || this.AllocatedUltimateRecoveries.Any() || this.AllocatedRecoveriesMerged.Count != 0;
+            }
+        }
     }
 }
