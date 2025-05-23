c8bb10b41b6cc896d168fad0c0c228c41055e14b
x Foote <xfoote@daleuw.com>
2025-05-23 15:58:23 +0100

    #271  USM fix and EventMetadata updates

--- Models/ORI/EventMetadataListModel.cs
+++ Models/ORI/EventMetadataListModel.cs
@@ -5,10 +5,12 @@ namespace Sandbox.Models.ORI
 	public class EventMetadataListModel
 	{
 		public string EventCode { get; set; }
+		public string EventStartDate { get; set; }
 		public string Peril { get; set; }
 		public string PerilRegion { get; set; }
 		public string EventDescription { get; set; }
 		public int NumClaims { get; set; }
 		public double IncurredUSDQtr { get; set; }
+		public int TotalCount { get; set; }
 	}
 }
