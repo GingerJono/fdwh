83bd7d4c8df980e2cc27f3a9707353be2b913356
Jono <jononeill@daleuw.com>
2025-11-07 14:46:13 +0000

    First attempt at the app

--- Program.cs
+++ Program.cs
@@ -135,6 +135,7 @@ builder.Services.AddScoped<ReservingService>();
 builder.Services.AddScoped<ReportingService>();
 builder.Services.AddScoped<ORIService>();
 builder.Services.AddScoped<PrismService>();
+builder.Services.AddScoped<ExceptionService>();
 builder.Services.AddScoped<CookieManager>();
 builder.Services.AddScoped<CytoraCheckingFilterState>();
 
