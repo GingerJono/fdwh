7a93e3e2eb4cbb938655ca00a69f88cce413e687
x Foote <xfoote@daleuw.com>
2025-11-11 15:12:51 +0000

    Premium updates

--- Models/Prism/AllocatedPremiumSignedModel.cs
+++ Models/Prism/AllocatedPremiumSignedModel.cs
@@ -3,10 +3,13 @@
     public class AllocatedPremiumSignedModel
     {
         public string ORIPolicyReference { get; set; } = string.Empty;
-        public string SettCcy { get; set; } = string.Empty;
-        public string RiskCode { get; set; } = string.Empty;
-        public string? Class { get; set; }
+        public string? Currency { get; set; } 
         public string? YOA { get; set; }
+        public string? Class { get; set; }
+        public string? ReservingClass { get; set; }
+        public string? RiskCode { get; set; } 
+        public string? DistributionChannel { get; set; } 
+        public string? Syndicate { get; set; }
         public string? LORSId { get; set; }
         public double? SignedPremium { get; set; }     
         public long RunID { get; set; }
--- Models/Prism/AllocatedPremiumWrittenModel.cs
+++ Models/Prism/AllocatedPremiumWrittenModel.cs
@@ -3,16 +3,14 @@
     public class AllocatedPremiumWrittenModel
     {
         public string ORIPolicyReference { get; set; } = string.Empty;
-        public string SettCcy { get; set; } = string.Empty;
-        public DateTime? PremiumWrittenDate { get; set; } 
-
-        public string? Class { get; set; }
+        public string Currency { get; set; } = string.Empty;
         public string? YOA { get; set; }
+        public string? Class { get; set; }
+        public string? ReservingClass { get; set; }
         public string? RiskCode { get; set; }
-
+        public string? DistributionChannel { get; set; }
+        public string? LORSId { get; set; }
         public double? WrittenPremium { get; set; }
-        
-
         public long RunID { get; set; }
     }
 }
